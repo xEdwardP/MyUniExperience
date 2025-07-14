c6e74bb6020543f81107c8b0563f0f01d598d3b5
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2025-07-13 18:42:32 -0600

    Add TourActivity

--- app/src/main/java/com/example/myuniexperience/tour/TourActivity.kt
+++ app/src/main/java/com/example/myuniexperience/tour/TourActivity.kt
@@ -1,34 +1,21 @@
 package com.example.myuniexperience.tour
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myuniexperience.R
-import com.google.android.material.button.MaterialButton
 
-class WelcomeActivity : AppCompatActivity() {
+class TourActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_tour)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val fullName = intent.extras?.getString("FULL_NAME").orEmpty()
-//        val fullName = intent.getStringExtra("FULL_NAME") ?: "Visitante"
-        findViewById<TextView>(R.id.tvWelcome).text = getString(R.string.WelcomeTitle, fullName)
-
-        val btnStartTour = findViewById<MaterialButton>(R.id.btnStartTour)
-
-        btnStartTour.setOnClickListener {
-            startActivity(Intent(this, TourActivity::class.java))
         }
     }
 }
